b04467b16576d5c71aa74f7070731c689018a9c3
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-17 00:54:29 +0200

    add: create users types

---
+++ src/types/user.types.ts
@@ -0,0 +1,16 @@
+export  type UserType = {
+    chatId: number
+    firstName: string | undefined
+    username: string | undefined
+    isActive: boolean
+}
+export type UserCreateModel = {
+    chatId: number
+    firstName: string
+    username: string
+}
+export type UserUpdateModel = {
+    chatId: number
+    firstName: string
+    username: string
+}
