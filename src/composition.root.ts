c040c6dd8376ef517af8730a38aeb8c10bb6a816
ivansukhoparov <rbcdaaa@gmail.com>
2024-08-18 15:34:16 +0200

    refactoring: add repositories to composition.root.ts

--- src/composition.root.ts
+++ src/composition.root.ts
@@ -1,8 +1,14 @@
 
 import {Container} from "inversify";
+import {AppSettings} from "./app.settings";
 import {PGAdapter} from "./base/pg.adapter";
+import {UsersRepository} from "./entities/users/users.repository";
+import {StatementsRepository} from "./entities/statments/statements.repository";
+
 
 
 export const container = new Container()
 
 container.bind<PGAdapter>(PGAdapter).to(PGAdapter).inSingletonScope()
+container.bind<UsersRepository>(UsersRepository).to(UsersRepository).inSingletonScope()
+container.bind<StatementsRepository>(StatementsRepository).to(StatementsRepository).inSingletonScope()
